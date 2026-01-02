86ef1a796014420c300deb4d0f5dc2c81e23669d
prabhat <kapkotiprabhat@gmail.com>
2026-01-02 16:28:49 +0530

    added the method level security

--- src/main/java/net/prabhat/todo_management/config/SpringSecurityConfig.java
+++ src/main/java/net/prabhat/todo_management/config/SpringSecurityConfig.java
@@ -2,8 +2,8 @@ package net.prabhat.todo_management.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,6 +14,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SpringSecurityConfig {
 
     @Bean
@@ -28,10 +29,10 @@ public class SpringSecurityConfig {
                 .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests(auth->{
-                    auth.requestMatchers(HttpMethod.POST,"/api/**").hasRole("ADMIN");
-                    auth.requestMatchers(HttpMethod.PUT,"/api/**").hasRole("ADMIN");
-                    auth.requestMatchers(HttpMethod.DELETE,"/api/**").hasRole("ADMIN");
-                    auth.requestMatchers(HttpMethod.GET,"/api/**").hasAnyRole("ADMIN","USER");
+//                    auth.requestMatchers(HttpMethod.POST,"/api/**").hasRole("ADMIN");
+//                    auth.requestMatchers(HttpMethod.PUT,"/api/**").hasRole("ADMIN");
+//                    auth.requestMatchers(HttpMethod.DELETE,"/api/**").hasRole("ADMIN");
+//                    auth.requestMatchers(HttpMethod.GET,"/api/**").hasAnyRole("ADMIN","USER");
                    // auth.requestMatchers(HttpMethod.GET,"/api/**").permitAll(); this exposes out rest API to all
                 })
                 .authorizeHttpRequests(auth -> auth
--- src/main/java/net/prabhat/todo_management/controller/TodoController.java
+++ src/main/java/net/prabhat/todo_management/controller/TodoController.java
@@ -6,6 +6,7 @@ import net.prabhat.todo_management.dto.TodoDto;
 import net.prabhat.todo_management.service.TodoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class TodoController {
     private TodoService todoService;
 
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping()
     public ResponseEntity<TodoDto> addTodo( @RequestBody TodoDto todoDto){
         TodoDto todo = todoService.addTodo(todoDto);
@@ -26,37 +28,42 @@ public class TodoController {
     }
 
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER'")
     @GetMapping("/{id}")
     public ResponseEntity<TodoDto> getTodo(@PathVariable Long id){
         TodoDto todoDto = todoService.getTodo(id);
         return new ResponseEntity<>(todoDto, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER'")
     @GetMapping()
     public ResponseEntity<List<TodoDto>> getAllTodo(){
         List<TodoDto> todos = todoService.getAllTodo();
         return new ResponseEntity<>(todos,HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<TodoDto> updateTodo(@RequestBody TodoDto todoDto,@PathVariable Long id){
         TodoDto updateTodo = todoService.updateTodo(todoDto,id);
         return ResponseEntity.ok(updateTodo);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteTodo(@PathVariable Long id){
         todoService.deleteTodo(id);
         return ResponseEntity.ok("Todo has Has been deleted Successfully! ");
     }
 
-
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER'")
     @PatchMapping("/{id}/complete")
     public ResponseEntity<TodoDto> completeTodo(@PathVariable Long id){
         TodoDto todoDto = todoService.completeTodo(id);
         return ResponseEntity.ok(todoDto);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER'")
     @PatchMapping("/{id}/inComplete")
     public ResponseEntity<TodoDto> inCompleteTodo(@PathVariable Long id){
         TodoDto todoDto = todoService.inComplete(id);
