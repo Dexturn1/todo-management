bb1aaef956db95acdcc84d6732185708ccb450b9
prabhat <kapkotiprabhat@gmail.com>
2026-01-02 17:00:41 +0530

    added the userRepository and roleRepositroy

---
+++ src/main/java/net/prabhat/todo_management/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package net.prabhat.todo_management.repository;
+
+import net.prabhat.todo_management.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+
+
+}
